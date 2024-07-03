d89fa9862054a3f686f27b4eac298fa5ef6f3eaa
Shrihari Magar <shriharimagar1@gmail.com>
2024-07-03 15:41:23 +0530

    feat: add TitleBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import { useState } from 'react'
 import "./App.css";
 import CardList from "./components/cards-list/cardList";
+import TitleBar from "./components/title-bar/TitleBar";
 
 function App() {
   return (
     <>
-      {/* <TitleBar/> */}
+      <TitleBar />
       <CardList />
     </>
   );
---
+++ src/components/title-bar/TitleBar.jsx
@@ -0,0 +1,12 @@
+import barBackground from "../../assets/bar-background.jpg";
+
+const TitleBar = () => {
+    return (
+        <div className="title-bar">
+            <img src={barBackground} alt="Background" className="title-bar-image" />
+            <div className="title-text julius-sans-one-regular">Wonders of Street View</div>
+        </div>
+    );
+};
+
+export default TitleBar;
